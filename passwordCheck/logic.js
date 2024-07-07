04b3f3310fe8c3132e58ece87ebadf46807fdbb2
TatendaMatoi <h200230b@hit.ac.zw>
2024-07-07 20:43:20 +0200

    modified the script

--- passwordCheck/logic.js
+++ passwordCheck/logic.js
@@ -2,14 +2,28 @@ let userName = prompt(
   "Hello, can you please enter your Name below and click ok."
 );
 
+while (userName === null) {
+  userName = prompt(
+    "You must enter a username. Please enter your Name below and click ok."
+  );
+}
+
 let password = prompt(
   `Greetings ${userName}, May you kindly enter your password below.`
 );
 
-while (password.includes(userName)) {
-  password = prompt(
-    `Greetings ${userName}, May you kindly choose a password that does not include your username.`
-  );
+while (password === null || password.includes(userName)) {
+  if (password === null) {
+    password = prompt("You must enter a password. Please try again.");
+  } else {
+    password = prompt(
+      `Greetings ${userName}, May you kindly choose a password that does not include your username.`
+    );
+  }
 }
 
-alert("Password is valid");
+if (password !== null) {
+  alert("Password is valid");
+} else {
+  alert("Password entry was cancelled.");
+}
